8cf54e47f909a08d020f240b71098e00c6f8f0a6
Refael <refael747@gmail.com>
2024-02-04 14:19:25 +0200

    style padding changing

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 // Description : A generic button, changed according to content, classname and onClick function
 // Props : content , className,  onClick
 // Creator : Refael
-export default function Button({ content="תוכן הכפתור",className="save",onClick=()=>{} }) {
+export default function Button({ content="תוכן ",className="save",onClick=()=>{} }) {
   return (
 <div>
           <button className={`${styles[className]} ${className}`} onClick={onClick}>{content}</button>
