4bc592ca70a40e5fedd21f06fcc6443f73e2ae05
ypreiser <ypreiser@gmail.com>
2024-02-04 15:49:02 +0200

    update text area by tzvi

--- src/components/InputTextArea/index.jsx
+++ src/components/InputTextArea/index.jsx
@@ -3,17 +3,9 @@ import styles from './style.module.css'
 // Description : generic input text area 
 // Props : value, onChange, placeholder, rows
 // Creator : yehoshua preiser
-export default function InputTextArea({ value, onChange, placeholder, rows=10 }) {
+export default function InputTextArea({ style = {},name={}, ...props }) {
   return (
-    <div className={styles.msgbox}>   
-    <p className={styles.msg}>הודעה</p>
-    <p className={styles.description}>זוהי ההודעה שתשלח בתזמון הנבחר</p>
-    <textarea className={styles.textarea}
-    value={value}
-    onChange={onChange}
-    placeholder={placeholder}
-    rows={rows}
-    />
-    </div>
+    <textarea className={styles.textarea} name={name} id={name} style={style} {...props} />
+    
   )
 }
