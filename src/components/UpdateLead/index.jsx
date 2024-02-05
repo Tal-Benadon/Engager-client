ba6b0b4dabcf7be8cb3139009ec62cf2db3ff43c
unknown <miriampuny16.com>
2024-02-05 16:03:45 +0200

    luli

---
+++ src/components/UpdateLead/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UpdateLead({userId}) {
+  return <div>
+    UpdateLead
+    </div>
+}
--- src/tests/Miryam.jsx
+++ src/tests/Miryam.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Icon from '../components/Icon'
+import UpdateLead from '../components/UpdateLead'
 
 export default function Miryam() {
   return <div>
-        <Icon nameIcon={'trash'}  nameColor  = {'active'}/>
+    <UpdateLead userId = {''}/>
+        {/* <Icon nameIcon={'trash'}  nameColor  = {'active'}/>
     <Icon nameIcon={'msgLeft'}  nameColor  = {'danger'}/>
     <Icon nameIcon={'pause'}/>
     <Icon nameIcon={'user'}/>
@@ -18,6 +20,6 @@ export default function Miryam() {
     <Icon nameIcon={'x'}/>
     <Icon nameIcon={'settingAlternativ'}/>
     <Icon nameIcon={'setting'}/>
-    <Icon nameIcon={'menu'} nameColor={"active"}/>
+    <Icon nameIcon={'menu'} nameColor={"active"}/> */}
     </div>
 }
