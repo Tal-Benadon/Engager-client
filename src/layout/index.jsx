2e8431c79cad51d818b42b2073647c1faac196a2
shirzemour <shirzcode@gmail.com>
2024-02-05 21:34:47 +0200

    loginPage rander login +register

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Route, Routes } from 'react-router';
 import LoginPage from '../pages/LoginPage';
-import Loading from '../components/Loading';
+
 
 export default function Layout() {
   return (
     <div>
       <Routes>
         <Route path='/login' element={<LoginPage />} />
-
       </Routes>
+        
     </div>
   )
 }
