3fe6a0c17f62cdf1165301302778f98e2f4b28b4
Refael <refael747@gmail.com>
2024-02-05 11:34:45 +0200

    description

--- src/pages/LeadInfoPage/index.jsx
+++ src/pages/LeadInfoPage/index.jsx
@@ -2,8 +2,17 @@ import styles from './style.module.css'
 
 
 
-// Description:
+// Description: This component serves as a user profile page. It is designed to display user information, including first name, last name, email, phone number, registration date, and active status indicator.
+//Use of this component should pass real user data from the DB as props.
 // Props:
+//   - firstName (string): The first name of the user.
+//   - lastName (string): The last name of the user.
+//   - phoneNumber (string): The phone number of the user.
+//   - email (string): The email address of the user.
+//   - notes (string): Additional notes or comments about the user.
+//   - signUpDate (string): The date of user registration.
+//   - isActive (boolean): A flag indicating whether the user is active.
+//    If isActive is true, it will display "פעיל"; if false, it will display a red dot and "לא פעיל"
 // Creator: Refael
 
 export default function LeadInfoPage({ firstName = "נוי", lastName = "כהן", phoneNumber = "054-8104093", email = "email@gmail.com", notes = "-", signUpDate = "12/03/22", isActive = false }) {
