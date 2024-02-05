83c0f1497644e40c15d4f43ea4871544b6578e2f
gila shouraky <gilasho1234@gmail.com>
2024-02-05 11:58:25 +0200

    TabSwitcher3

--- src/components/TabSwitcher/index.jsx
+++ src/components/TabSwitcher/index.jsx
@@ -4,17 +4,16 @@ import styles from './style.module.css'
 // Description : 
 // Props : ____________ , _________
 // Creator : ________
-export default function TabSwitcher() {
-  const classHeader = ({ isActive }) => isActive ? styles.active : ""
+export default function TabSwitcher({ rout = [] }) {
 
   return (
     <div className={styles.allcom}>
-      <div className={styles.TabSwitcher}>
-        <Outlet />
-        <NavLink to="/gila1" className={styles.active}>הודעות</NavLink>
-        <NavLink to="/gila2" className={styles.active}>נרשמים</NavLink>
-      </div>
-        <div className={styles.line}></div>
+      {rout.map(r =>
+        <div className={styles.TabSwitcher}>
+          <NavLink to={`/ ${r.tab}`} className={styles.active}>{r.text}</NavLink>
+        </div>
+      )}
+      <div className={styles.line}></div>
     </div>
   )
 }
