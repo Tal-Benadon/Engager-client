c81389ecf772ea8fbd20ae62f9628b9156d972b1
gila shouraky <gilasho1234@gmail.com>
2024-02-05 19:04:58 +0200

    MessagePage

--- src/components/HeadLine/index.jsx
+++ src/components/HeadLine/index.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import styles from './style.module.css'
 import Icon from '../Icon'
+import PopUp from '../PopUp'
 //קומפוננטה שצריכה לקבל כותרת וכותרת משנה כ פרופס וכן אייקון
+
 export default function HeadLine({ title, icon, dateCreate }) {
+  const editMessage = () => alert("ghjfkhjf")
+
   return (
     <div className={styles.headLine} >
       <div >
         <p className={styles.title} >{title}</p>
         <p className={styles.subtitle} >נוצר ב {dateCreate}</p>
       </div>
-      <div onClick={()=>{alert('hi')}}><Icon className={styles.button} nameIcon={"writing"} nameColor={''} /></div>
+      <div onClick={editMessage}><Icon className={styles.button} nameIcon={"writing"} nameColor={''} /></div>
     </div>
   )
 }
--- src/components/MessagePage/index.jsx
+++ src/components/MessagePage/index.jsx
@@ -4,36 +4,8 @@ import styles from './style.module.css'
 import Accordion from '../Accordion'
 
 export default function MessagePage({ dateCreate, title, icon, message, dateSend, timeSend }) {
-    // const [isEditing, setIsEditing] = useState(false);
-    // const [textValue, setTextValue] = useState('הודעה');
 
-    // const handleEditClick = () => {
-    //     setIsEditing(true);
-    // };
-
-    // const handleInputChange = (event) => {
-    //     setTextValue(event.target.value);
-    // };
-
-    // const handleSaveClick = () => {
-    //     setIsEditing(false);
-    //     // כאן תוכל לעדכן את המידע בעסקים כמו שאתה רוצה
-    // };
     return (
-        <>
-            {/* <div>
-                {isEditing ? (
-                    <input
-                        type="text"
-                        value={textValue}
-                        onChange={handleInputChange}
-                        onBlur={handleSaveClick}
-                        autoFocus
-                    />
-                ) : (
-                    <button onClick={handleEditClick}>{textValue}</button>
-                )}
-            </div> */}
             <div className={styles.MessagePage}>
                 <HeadLine dateCreate={dateCreate} title={title} icon={icon}/>
                 <div className={styles.message}>
@@ -48,6 +20,5 @@ export default function MessagePage({ dateCreate, title, icon, message, dateSend
                     {['hello', 'i', 'am', 'shaked', 'ben', 'hamo', 'guikh', 'gyhjbvh']}
                 </Accordion>
             </div>
-        </>
     )
 }
