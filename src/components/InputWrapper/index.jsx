4e3e1e567991cad480e292559991fa3d79615744
yakovMoshel <yakovmoshel123@gmail.com>
2024-02-05 10:21:07 +0200

    function InputWrapper

--- src/components/InputWrapper/index.jsx
+++ src/components/InputWrapper/index.jsx
@@ -5,14 +5,15 @@ import styles from './style.module.css'
 // Creator : yakov moshel   
 
 // קריאה לפונקציה
-// <InputWrapper label="שם:" to=""  h2=" " type="text" value={name}/>
+// <InputWrapper label="שם:" to=""  h2=" " children={} type="text" value={name}/>
 
 
-export default function InputWrapper({label,h2,to}) {
+export default function InputWrapper({ label, h2, to, children }) {
     return (
         <div className={styles.InputWrapper}>
             <h2 className={styles.h2} >{h2}</h2>
-            <label className={styles.label} to ={to}>{label}</label>
+            <label className={styles.label} to={to}>{label}</label>
+            {children}
         </div>
     );
 }
