6779da3005941a4a0cf34a1c19d03b8a6060808f
mayyan12 <katzmyyn@gmail.com>
2024-02-06 21:38:15 +0200

    pop 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Layout from './layout'
+import Maayan from './tests/Maayan'
+import Popover from './components/Popover'
 
 export default function App() {
   return (
     <div>
-      <Layout />
+      <Maayan />
+      {/* <Layout /> */}
+      {/* <Popover /> */}
     </div>
   )
 }
--- src/tests/Maayan.jsx
+++ src/tests/Maayan.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import Popover from '../components/Popover'
+import '../Index.css'
 export default function Maayan() {
+  let list = [
+    { text: "get out", icon: '💢' },
+    { text: "change profile", icon: '💨' },
+    { text: "deleted", icon: '🗑' },
+    { text: "get d", icon: '💢' },
+    { text: "change profdile", icon: '💨' },
+    { text: "delete", icon: '🗑' }
+  ];
+
   return (
-    <div>maayan</div>
+
+    <div className='mayanMain' >
+      {/* <h1 >user pop</h1> */}
+      <Popover list={list} >
+        click
+      </Popover>
+
+    </div>
   )
 }
