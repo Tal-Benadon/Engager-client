140fb0877de6a836ca8339c2e99412a9b58939fb
IsraBA <IsraelBenAri1000@gmail.com>
2024-02-06 11:46:29 +0200

    css improvements

--- src/components/CampaignList/index.jsx
+++ src/components/CampaignList/index.jsx
@@ -5,10 +5,8 @@ import styles from './style.module.css'
 export default function CampaignList({ campaignList }) {
     return (
         <div className={styles.campaignList}>
-            <div className={styles.list}>
                 {campaignList
                      .map(c => <CampaignItem key={c.id} id={c.id} title={c.title} />)}
-            </div>
         </div>
     )
 }
