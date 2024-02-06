ee6481da4605171586528072a6d188f11ef75fc1
yakovMoshel <yakovmoshel123@gmail.com>
2024-02-06 14:48:34 +0200

    finish new message Form

--- src/tests/Gila.jsx
+++ src/tests/Gila.jsx
@@ -14,8 +14,14 @@ export default function Gila() {
   return (
     <div>
       {/* <TabSwitcher/> */}
+<<<<<<< Updated upstream
       {/* <CampaignItem/> */}
       <MessagePage title={title} dateCreate={dateCreate} icon={"icon"} message={message} dateSend={dateSend} timeSend={timeSend} />
+=======
+      {/* <Outlet/> */}
+      <CampaignItem/>
+
+>>>>>>> Stashed changes
     </div>
   )
 }
