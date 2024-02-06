4b8b0e22e01c834c568bbbcbb1a1f514262ae9d5
yakovMoshel <yakovmoshel123@gmail.com>
2024-02-06 14:26:29 +0200

    finish new message Form

--- src/components/NewMessageForm/index.jsx
+++ src/components/NewMessageForm/index.jsx
@@ -1,80 +1,83 @@
-import styles from './style.module.css'
-import axios, { Axios } from 'axios';
+import styles from "./style.module.css";
+import axios, { Axios } from "axios";
 
-
-import InputWrapper from '../InputWrapper';
-import Button from '../Button';
-import InputText from '../InputText/InputText';
-import InputTextArea from '../InputTextArea/index';
+import InputWrapper from "../InputWrapper";
+import Button from "../Button";
+import InputText from "../InputText/InputText";
+import InputTextArea from "../InputTextArea/index";
 
 import { FaTimes } from "react-icons/fa";
-import { useState } from 'react';
+import { useState } from "react";
 
-
-
-// Description : 
+// Description :
 // Props : ____________ , _________
-// Creator : ________   
+// Creator : ________
 
+export default function NewMassageForm({ setIsOpen }) {
+  const [subject, setSubject] = useState("");
+  const [content, setContent] = useState("");
 
-export default function NewMassageForm({setIsOpen}) {
-    const [subject, setSubject] = useState('')
-    const [content, setContent] = useState('')
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const submmit = { subject, content };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const submmit = { subject, content };
-    
-        try {
-            const response = await axios.post('http://localhost:2500/campaign/65c0939a5aa397278552a5b5/msg', submmit, {
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            setIsOpen(false)
-            console.log(response.data);
-            console.log(subject, content);
-        } catch (error) {
-            console.error("Error:", error);
+    try {
+      const response = await axios.post(
+        "http://localhost:2500/campaign/65c0939a5aa397278552a5b5/msg",
+        submmit,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-        console.log(subject, content);
-    };
-    
+      );
+      setIsOpen(false);
+      console.log(response.data);
+      console.log(subject, content);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    console.log(subject, content);
+  };
 
+  return (
+    <div className={styles.InputWrapper}>
+      <hr />
+      <form onSubmit={handleSubmit}>
+        <main className={styles.main}>
+          <InputWrapper
+            label="שם הודעה"
+            subLabel="שם פנימי שיהיה חשוף רק לך"
+            to={"msgName"}
+            children={
+              <InputText
+                name={"msgName"}
+                onChange={(e) => setSubject(e.target.value)}
+              />
+            }
+            type="text"
+          ></InputWrapper>
 
-    return (
-        <div className={styles.InputWrapper}  >
-            <hr />
-            <form onSubmit={handleSubmit} >
-                <main className={styles.main}>
-                    <InputWrapper
-                        label="שם הודעה"
-                        subLabel="שם פנימי שיהיה חשוף רק לך"
-                        to={"msgName"}
+          <InputWrapper
+            label="הודעה"
+            subLabel="זוהי  ההודעה שתשלח בתזמון הנבחר"
+            to={"msgContent"}
+            children={
+              <InputTextArea
+                name={"msgContent"}
+                onChange={(e) => setContent(e.target.value)}
+              />
+            }
+            type="text"
+          />
+        </main>
+        <hr />
 
-                        children={<InputText name={"msgName"} onChange={(e) => setSubject(e.target.value)} />}
-                        type="text"
-                    >
-
-
-                    </InputWrapper>
-
-                    <InputWrapper
-                        label="הודעה"
-                        subLabel="זוהי  ההודעה שתשלח בתזמון הנבחר"
-                        to={"msgContent"}
-                        children={<InputTextArea name={"msgContent"} onChange={(e) => setContent(e.target.value)} />}
-                        type="text"
-                    />
-                </main>
-                <hr />
-
-                <div className={styles.actions}  >
-                    <Button className={"save"} content={"שמירה"} />
-                    <Button className={"cancel"} content={"ביטול"} />
-                </div>
-            </form>
+        <div className={styles.actions}>
+          <Button className={"save"} content={"שמירה"} />
+          <Button className={"cancel"} content={"ביטול"} />
         </div>
-    )
-
+      </form>
+    </div>
+  );
 }
