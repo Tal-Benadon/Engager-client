fa8b2592a81acd7d0c4b47cffd0c3f5831fc8396
gila shouraky <gilasho1234@gmail.com>
2024-02-06 13:13:29 +0200

    edit messagepage

--- src/components/HeadLine/index.jsx
+++ src/components/HeadLine/index.jsx
@@ -5,6 +5,7 @@ import PopUp from '../PopUp'
 import MessagesTab from '../MsgTab'
 import NewMassageForm from '../NewMessageForm'
 import MessageEdit from '../MessageEdit'
+import DataContext from '../../context/DataContext'
 //קומפוננטה שצריכה לקבל כותרת וכותרת משנה כ פרופס וכן אייקון
 
 export default function HeadLine({ title, icon, dateCreate, children }) {
