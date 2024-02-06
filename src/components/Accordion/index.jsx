6ea29284924e109e00a4335529179ada5ba179eb
shaked <shaked4041@gmail.com>
2024-02-06 17:15:27 +0200

    accordion style changes

--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -3,38 +3,38 @@ import styles from './style.module.css';
 import Icon from '../Icon'
 import LeadItem from '../LeadItem';
 import formatDate from '../../functions/DateFormat';
-// import { ToastContainer, toast } from 'react-toastify';
-// import 'react-toastify/dist/ReactToastify.css';
-
 
 export default function Accordion({ title, children, campaignId }) {
   const [isOpenAcord, setIsOpenAcord] = useState(false);
-  // const notify = () => toast("Wow so easy!");
+
+  const toggleAccordion = () => {
+    setIsOpenAcord(prevState => !prevState); // Toggle the state
+  };
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <div className={styles.header} onClick={() => setIsOpenAcord(!isOpenAcord)}>
-          <div>{isOpenAcord ? <Icon nameIcon={'x'} nameColor={''} /> : <div className={styles.open}><Icon className={styles.icon} nameIcon={'enlarge'} nameColor={''} />{'הצג הכל'}</div>}</div>
+        <div className={styles.header} onClick={toggleAccordion}>
+
+          <div>
+            {isOpenAcord ? <Icon nameIcon={'x'} nameColor={''} /> : <div className={styles.open}><Icon className={styles.icon} nameIcon={'enlarge'} nameColor={''} />{'הצג הכל'}</div>}
+          </div>
           <span className={styles.title}>{title}</span>
         </div>
-        {isOpenAcord && (
-          <div className={styles.content}>
-            {children.map((lead, index) => (
-              <div className={styles.single} key={index}>
-                <LeadItem
-                  campaignId={campaignId}
-                  name={"lead.name"}
-                  email={lead.email}
-                  date={formatDate(lead.receptionDate)}
-                  id={lead._id} />
-                {console.log(lead.name)}
-              </div>
-            )
-            )}
-          </div>
-        )}
+        <div className={`${styles.content} ${!isOpenAcord ? styles.hide : ''}`}>
+          {children.map((lead, index) => (
+            <div className={styles.single} key={index}>
+              <LeadItem
+                campaignId={campaignId}
+                name={"lead.name"}
+                email={lead.email}
+                date={formatDate(lead.receptionDate)}
+                id={lead._id} />
+              {console.log(lead.name)}
+            </div>
+          ))}
+        </div>
       </div>
-    // </div>
+    </div>
   );
 }
