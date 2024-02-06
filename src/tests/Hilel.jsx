11534e306e388c7d82cc8bfabf56158a9aa9aa7e
HillelChen <hillel.chen@gmail.com>
2024-02-06 10:43:28 +0200

    try

--- src/tests/Hilel.jsx
+++ src/tests/Hilel.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import InputText from '../components/InputText/InputText'
 import UserLogoutEdit from '../components/userLogoutEdit'
+import Register from '../components/Register'
 export default function Hilel() {
   return (
     <>
       {/* < Login /> */}
       {/* < InputText /> */}
-      <UserLogoutEdit />
+      {/* <UserLogoutEdit /> */}
+      <Register />
     </>
   )
 }
