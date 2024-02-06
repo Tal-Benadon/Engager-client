8521041d2f7519e836f8af2c5234eabe1da3a1b8
IsraBA <IsraelBenAri1000@gmail.com>
2024-02-06 18:22:27 +0200

    css improvements

--- src/components/PopUp/index.jsx
+++ src/components/PopUp/index.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styles from './style.module.css';
 import Icon from '../Icon'
+import DataContext from '../../context/DataContext';
 
-export default function PopUp({ setIsOpen, isOpen, children}) {
+export default function PopUp() {
 //the function needs to recive children and need to recive isOpen and setIsOpen in the props and render them
-  // const [isOpen, setIsOpen] = useState(false);
-
+  // const [isOpen, setIsOpen] = useState9(false);
+  const { setIsOpen, isOpen} = useContext(DataContext)
+console.log({isOpen});
   return (
     <div className={styles.allpopup}>
       {isOpen && (
@@ -16,7 +18,7 @@ export default function PopUp({ setIsOpen, isOpen, children}) {
                 <Icon nameIcon={'x'} nameColor={''}/>
               </span>
             </div>
-            {children}
+            {isOpen}
           </div>
         </div>
       )}
