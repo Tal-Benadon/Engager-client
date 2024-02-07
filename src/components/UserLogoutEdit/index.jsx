51fc927faaa631772ced968a5dfc6502d7482721
liel <lielcohen213@gmail.com>
2024-02-07 18:08:22 +0200

    userlogout

--- src/components/UserLogoutEdit/index.jsx
+++ src/components/UserLogoutEdit/index.jsx
@@ -5,15 +5,15 @@ import ThreeDots from '../ThreeDotsMenu'
 import Popover from '../Popover'
 import Login from '../Login/Login'
 import { useNavigate } from 'react-router-dom'
-
+//מקבלת פרטי יוזר כאוביקט משתמשת בשם והתמונה אם אין תמונה אז יש דיפולט אייקון יוזר
+//בלחיצה על החץ נפתחת רשימה עם שתי אפשריות
+//1 יציאה-מעביר לדף לוגאין ומוחק מלוקאל סטורג את הטוקן
+//2 עריכה-כרגע לא פעיל
 
 
 
 export default function UserLogoutEdit({ user = { name: "user", avatar: undefined } }) {
     const navigate = useNavigate()
-
-
-    
 
     let list = [{
         text: "עריכה",
@@ -22,13 +22,17 @@ export default function UserLogoutEdit({ user = { name: "user", avatar: undefine
         text: "התנתקות",
         icon: (<Icon nameIcon={'x'} nameColor={''} />),
         onClick: () => {
+            delete localStorage.token;
             navigate(`/login`,{relative:'path'})
         }
     }]
     return (
         <>
             <button className={styles.userInfo}  >
-                <div> <img src={user.avatar} className={styles.pictuare} /> </div>
+                <div> 
+                  {user.avatar ? <img src={user.avatar} className={styles.pictuare} /> :
+                  <Icon nameIcon={'user'} nameColor={''} />}
+                    </div>
                 <div className={styles.user}> {user.name} </div>
                 <Popover list={list} fnName={"onClick"}>
                     <div className={styles.icon} > <Icon nameIcon={'chevronDown'} nameColor={''} /></div>
