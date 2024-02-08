74819e90afb1f0f339efe506ccda4cff709e14db
IsraBA <IsraelBenAri1000@gmail.com>
2024-02-08 12:16:35 +0200

    css improvements

--- src/components/MsgTab/index.jsx
+++ src/components/MsgTab/index.jsx
@@ -9,23 +9,23 @@ import DataContext from '../../context/DataContext';
 import Popover from '../Popover';
 import Icon from '../Icon';
 
-export default function  MsgTab() {
-  const { campaign } =useCampaign();
-  if(!Object.keys(campaign).length) return <></>
+export default function MsgTab() {
+  const { campaign } = useCampaign();
+  if (!Object.keys(campaign).length) return <></>
 
   return (
     <div className={styles.msgTab}>
       <HeadLine
         title={campaign.title}
         subtitle={`${campaign.leads.length} נרשמים, ${campaign.msg.length} הודעות`}
-       />
+      />
       <TabSwitcher rout={[
         { tab: `campaign/${campaign._id}/leads`, text: `נרשמים(${campaign.leads.length})` },
         { tab: `campaign/${campaign._id}/messages`, text: "הודעות" }
       ]} />
       <MsgListHolder />
       <div className={styles.menu}>
-      <Popover fnName={"onClick"} list={[
+        <Popover fnName={"onClick"} list={[
           {
             text: "עריכת רשימה",
             icon: <Icon nameIcon={"writing"} />
