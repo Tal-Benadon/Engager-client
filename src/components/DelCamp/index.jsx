b69466b9609476bab4b24776f6396cf03879a430
gila shouraky <gilasho1234@gmail.com>
2024-02-08 15:57:38 +0200

    delcamp

--- src/components/DelCamp/index.jsx
+++ src/components/DelCamp/index.jsx
@@ -3,24 +3,43 @@ import { useParams } from 'react-router'
 import api from '../../functions/api'
 import Button from '../Button'
 import styles from './style.module.css'
+import { useContext, useState } from 'react';
+import DataContext from '../../context/DataContext'
+
 
 // Description : 
 // Props : ____________ , _________
 // Creator : ________
 export default function DelCamp() {
+    const { isOpen, setIsOpen } = useContext(DataContext);
+    //   const [campaign, setCampaign] = useState([])
+    //   const getCamp = () => {
+    //       api.get("/campaign")
+    //         .then(res => {
+    //           console.log("campaign:", res)
+    //           // nav(`campaign/${res[0]._id}`)
+    //           setCampaign(res)
+    //         })
+    //         .then()
+    //     }
+
     const campId = location.pathname.split('/')[2]
-    console.log({campId})
-    const delCampaign=()=>{
-        api.del( `/campaign/${campId}`)
-        .then(res=>setIsOpen(false))
+    console.log({ campId })
+    const url = `/campaign/${campId}`;
+    const delCampaign = () => {
+        api.del(url)
+            .then(res => setIsOpen(false))
+
+        // .then(getCamp())
 
     }
+
     return (
         <div>
             "האם אתה בטוח שברצונך למחוק רשימה זו לצמיתות?"
             <div className={styles.actions}>
-                <Button className={"cancel"} content={"ביטול" } onClick={() => setIsOpen(false)}/>
-                <Button className={"save"} content={"שמירה"} onClick={delCampaign}/>
+                <Button className={"cancel"} content={"ביטול"} onClick={() => setIsOpen(false)} />
+                <Button className={"save"} content={"שמירה"} onClick={delCampaign} />
             </div>
         </div>
     )
