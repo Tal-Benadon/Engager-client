8fccdbf310e90dd1bbe1701045cef34f5ed3dca9
YakovMoshel <yakovmoshel123@gmail.com>
2024-03-10 16:35:33 +0200

    translate function

--- src/components/CheckBox/index.jsx
+++ src/components/CheckBox/index.jsx
@@ -13,16 +13,16 @@ export default function CheckBox() {
     <div className={styles.CheckBoxFunction}>
       <div onClick={handleCheckBoxChange} className={styles.svg}>
         <svg
+        className={styles.vi}
           width="12"
           height="10"
           viewBox="0 0 12 10"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           >
-          {isChecked ? 
-           
-          <path className={styles.vi}
-            d="M10.8002 1.40002L3.64068 8.60002L1.2002 6.14574"
+          {isChecked ?          
+           <path 
+           d="M10.8002 1.40002L3.64068 8.60002L1.2002 6.14574"
             stroke="#6B6B6B"
             stroke-width="2"
             stroke-linecap="round"
---
+++ src/functions/translate.js
@@ -0,0 +1,25 @@
+
+export default function Translet(word) {
+    let TransletWords = {
+      phone: 'טלפון',
+      email: 'אימייל',
+      campaign: 'קמפיין',
+      username: 'שם משתמש',
+      creation: 'יצירה',
+      date: 'תאריך',
+      id: 'מזהה',
+      title: 'כותרת',
+      details: 'פרטים',
+      isActive: 'פעיל',
+      isOnline: 'סטטוס',
+      msg: 'הודעה',
+      lead: 'מוביל',
+      joinDate: 'תאריך הצטרפות',
+      messagesSent: 'הודעות שנשלחו'
+    };
+  
+    // מציאת התרגום המתאים לפי הפרופרטי שהועבר
+    return  TransletWords[word] || '';
+  }
+
+  
