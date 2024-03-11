4563b8eefc8def44bc652368370123bdff391e5b
ypreiser <ypreiser@gmail.com>
2024-03-11 12:47:45 +0200

    webhook2

--- src/components/WebHook/index.jsx
+++ src/components/WebHook/index.jsx
@@ -3,27 +3,45 @@ import styles from './style.module.css'
 import Icon from '../Icon'
 import api from '../../functions/api'
 import axios from 'axios'
-export default function WebHook({ campaign_id }) {
-    const [link, setLink] = useState()
-    // useEffect(() => {
-    //     // axios -לשלוח את התז של הקמפיין , ליצור טוקן, ןלהחזיר אותו לתוך המשתנה
-    //     // api.post('/webhook' , campaign_id)
-    //     axios.post('http://localhost:2500/webhook', { campaign_id })
-    //         .then(res => setLink('https://www.engager.co.il/webhook/' + res.data))
+import Button from '../Button'
+import { toast } from "react-toastify";
 
-    // }, [])
+export default function WebHook({ campaign_id }) {
+    const [link, setLink] = useState('65c9d1ae431dbc73b311ee14')
+
+    
+    const createWebHook = async () => {
+        try {
+            const res = await api.post('/webhook', { campaign_id })
+            setLink('https://www.engager.co.il/webhook/' + res.data)
+        } catch (error) {
+            console.error('Error creating webhook:', error)
+        }
+    }
+
+
+    useEffect(() => {
+        createWebHook()
+    }, [campaign_id])
+
+
     function copy() {
         navigator.clipboard.writeText(link)
+        toast.success("הקישור הועתק בהצלחה!");
     }
+
+
     return <div className={styles.container}>
         <p className={styles.titele}>Webhook</p>
         <p className={styles.pas}>זוהי הכתובת של המנגנון שמאפשר לנו לקבל את פרטי הנרשמים מהטופס שלך, ברגע שמתבצעת הרשמה</p>
         <div className={styles.link}>
-            <p className={styles.pLink}>{link}</p>
             <div className={styles.icon} onClick={copy}>
                 <Icon nameIcon={'copy'} />
             </div>
+            <p className={styles.pLink}>{link || 'אין קישור'}</p>
         </div>
+        <Button content='העתקת כתובת קישור' className='fill' onClick={copy} />
+        <Button content='ייצר קישור חדש' className='fill' onClick={createWebHook} />
     </div>
 }
 
