8b054eab564443a7c62cdf04435f35912c9e0095
Tal Ben Adon <talbenadon2@gmail.com>
2024-03-11 19:18:34 +0200

    added more logic that needs to be confirmed

--- src/pages/ActivateAccount/index.jsx
+++ src/pages/ActivateAccount/index.jsx
@@ -1,19 +1,39 @@
 import React, { useContext, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import api from '../../functions/api';
 import DataContext from '../../context/DataContext';
 
 //This component is rendered when a user presses a link that he gets on what'sapp after he first registers his account. 
 //logic in the server will make the user {isActive:True} if the token is not expired and information is correct.
 export default function ActivateAccount() {
+    nav = useNavigate()
     const { user } = useContext(DataContext)
     console.log(user);
     const { userToken } = useParams()
     console.log(userToken);
     useEffect(() => {
-        const response = api.post(`/user/activate/${userToken}`)
+        const activateUserApiCall = async () => {
+            const response = await api.post(`/user/activate/${userToken}`)
+            console.log(response.success);
+            if (response.success === true) {
+                console.log(response.msg);
+                // - activate when all checks are complete, for now we
+                // setTimeout(() => {
+                //     nav('/')
+                // }, 2500);
+
+                //OR
+
+                //setTimeout(() => {
+                //localStorage.removeItem("token");
+                //nav('/login')
+                //}, 2500)
+            }
+        }
+        activateUserApiCall()
     }, [])
 
+    //TODO - ADD LOADING ANIMATION? DECIDE ON THE 2.5 SECONDS OF WHAT THE USER SEES AFTER CONFIRMATION
     return (
         <div>
             Activating Account
