185daeda5a0989a2f4d37c90962329af5c179b07
tehilla <tehilla183@gmail.com>
2024-03-12 13:30:18 +0200

    delet lead from camp

--- src/tests/Teliha.jsx
+++ src/tests/Teliha.jsx
@@ -1,12 +1,25 @@
+
+
 import React from 'react'
+import Popover from '../components/Popover'
+import '../Index.css'
+import Icon from '../components/Icon';
 
 export default function Teliha() {
+  let list = [
+    { text: "הסר מהרשימה", icon:< Icon nameIcon={"trash"}/> , onClick: () => {} },
+
+  ];
+
   return (
-    <div>
-      Teliha
-      function
-      TO
-      test
+
+    <div className='mayanMain' >
+      {/* <h1 >user pop</h1> */}
+      <Popover list={list} fnName='onRight'  >
+        click
+      </Popover>
+
     </div>
   )
 }
+
