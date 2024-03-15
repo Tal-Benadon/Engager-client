f253d921fb9cfdd82649f1dcc9cc4b72b19239cf
gila shouraky <gilasho1234@gmail.com>
2024-03-15 15:33:15 +0200

    feedback

--- src/components/FeedBack/index.jsx
+++ src/components/FeedBack/index.jsx
@@ -1,30 +1,23 @@
 import styles from "./style.module.css";
-import axios, { Axios } from "axios";
 import InputWrapper from "../InputWrapper";
-import Button from "../Button";
 import InputText from "../InputText/InputText";
 import InputTextArea from "../InputTextArea/index";
 import { toast } from 'react-toastify';
 import React, { useState } from "react";
+import { useParams } from 'react-router'
 import { useNavigate } from "react-router";
+import api from "../../functions/api";
 
 export default function FeedBack() {
-
-  const [user, setUser] = useState("");
-  const [message, setMessage] = useState("");
+  const { user } = useParams()
+  const [content, setContent] = useState("");
   const nav = useNavigate()
 
   const handelSubmitNewFeedBack = async (e) => {
     e.preventDefault();
-    const SubmmitNewFeedBack = {
-      user: _id,
-      message
-    };
     try {
-      const response = await axios.post(
-        // "http://localhost:2500/campaign",
-
-        SubmmitNewFeedBack,
+      const response = await api.post(
+        `/feedback/${user}`, content ,
         {
           headers: {
             "Content-Type": "application/json",
@@ -53,13 +46,13 @@ export default function FeedBack() {
           <div className={styles.title}>דברו איתנו</div>
           <div>
             <InputWrapper label={"כותרת"} >
-              <InputText name={'title'} required={true} onChange={(e) => setMessage(e.target.value)} className={styles.input} />
+              <InputText name={'title'} required={true} onChange={(e) => setContent(e.target.value)} className={styles.input} />
             </InputWrapper>
           </div>
 
           <div >
             <InputWrapper label={"תוכן ההודעה"} className={styles.nameinput}>
-              <InputTextArea name={"תוכן ההודעה"} onChange={(e) => setMessage(e.target.value)} className={styles.inputaria} rows="5"
+              <InputTextArea name={"content"} onChange={(e) => setContent(e.target.value)} className={styles.inputaria} rows="5"
               />
             </InputWrapper>
           </div>
