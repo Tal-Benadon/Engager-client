732257e23101d3569fe0d412ce8968781fec761e
roni sa <roni1900@gmail.com>
2024-03-20 10:53:17 +0200

    another fix to the popover

--- src/components/Popover/index.jsx
+++ src/components/Popover/index.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import style from "./style.module.css";
-export default function Popover({ list, children, fnName,}) {
-    const maxHeight = (list.length * 50) + 20;
-    const [isClicked, setIsClicked] = useState(false);
+export default function Popover({ list, children, fnName, }) {
+    const maxHeight = (list.length * 50) + 20
+    const [isClicked, setIsClicked] = useState(false)
 
 
 
@@ -36,7 +36,7 @@ export default function Popover({ list, children, fnName,}) {
 
     function handleClick(e) {
         e.preventDefault()
-        let screenWidth = window.screen.width;
+        let screenWidth = window.screen.width
         let screenHeight = window.screen.height;
         let finalStyle = { display: 'flex' }
         let elementWidth = e.screenX;
@@ -44,15 +44,18 @@ export default function Popover({ list, children, fnName,}) {
         let elmentHeight = e.screenY;
         let totalHeight = screenHeight - elmentHeight;
         if (isClicked) return setIsClicked(false)
+
+
         if (totalWidth > 210) {
             finalStyle.left = 15;
         }
         else {
             finalStyle.right = 15;
         }
-        if (e.pageY > 600) {
+        if(e.pageY>100){
             finalStyle.bottom = 15;
         }
+
         else if (totalHeight > maxHeight) {
             finalStyle.top = 15
 
@@ -64,32 +67,32 @@ export default function Popover({ list, children, fnName,}) {
     }
 
     return (
-
+        
         <div className={style.mainIcon} {...finalFunction} >
             <button className={style.children} >
                 {children}
             </button>
             {!isClicked ? ""
 
-                :
-
+                : 
+                
                 <div className={style.Popover} style={isClicked} >
-                    <div className={style.container}>
-                        <ul >
-                            {list?.map?.(item => (
-                                <li className={item.color === "red" ? style.red : style.green}
-                                    onClick={item.onClick} key={item.text} >
-                                    <span className={style.icon}>{item.icon}</span>
-                                    <span className={style.text}>{item.text}</span>
-                                </li>
-                            ))}
-                        </ul>
+                    <div  >
+                    <ul >
+                        {list?.map?.(item => (
+                            <li className={item.color === "red" ? style.red : style.green}
+                                onClick={item.onClick} key={item.text} >
+                                <span className={style.icon}>{item.icon}</span>
+                                <span className={style.text}>{item.text}</span>
+                            </li>
+                        ))}
+                    </ul>
                     </div  >
                 </div>
-
+              
             }
         </div>
-
+ 
     );
 }
 
