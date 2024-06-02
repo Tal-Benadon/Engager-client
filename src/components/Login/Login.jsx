d9fefcc275c53413c848dcf3e12bf48b2406a47e
kobikru <75983501+kobikru@users.noreply.github.com>
2024-06-02 14:44:02 +0300

    email to lower case

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,96 +1,115 @@
-import React, { useContext } from 'react'
-import styles from './style.module.css'
-import InputText from '../InputText/InputText'
-import Button from '../Button'
-import InputWrapper from '../InputWrapper'
-import { useState } from 'react'
-import axios from 'axios'
-import api from '../../functions/api'
-import DataContext from '../../context/DataContext'
-import { useNavigate } from 'react-router'
-import getGoogleOAuthURL from '../../functions/loginWithGoogle'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import React, { useContext } from "react";
+import styles from "./style.module.css";
+import InputText from "../InputText/InputText";
+import Button from "../Button";
+import InputWrapper from "../InputWrapper";
+import { useState } from "react";
+import axios from "axios";
+import api from "../../functions/api";
+import DataContext from "../../context/DataContext";
+import { useNavigate } from "react-router";
+import getGoogleOAuthURL from "../../functions/loginWithGoogle";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 // login page.
 // <button /> gets props of content. needs to get ruot to DB to check that user fits password
 
 export default function Login() {
-    const notify = () => toast(`Wrong username or password`);
+  const notify = () => toast(`Wrong username or password`);
 
-    const [formState, setFormState] = useState({})
-    const { user, setUser } = useContext(DataContext)
-    const nav = useNavigate()
-    async function handleSubmit(e) {
-        try {
-            e.preventDefault();
-            const { token, user } = await api.post("/login", formState);
-            setUser(user)
+  const [formState, setFormState] = useState({});
+  const { user, setUser } = useContext(DataContext);
+  const nav = useNavigate();
+  async function handleSubmit(e) {
+    try {
+      e.preventDefault();
+      const { token, user } = await api.post("/login", formState);
+      setUser(user);
 
-            localStorage.token = token
-            nav("/")
-        } catch (err) {
-            console.error(err);
-            notify();
-        }
+      localStorage.token = token;
+      nav("/");
+    } catch (err) {
+      console.error(err);
+      notify();
     }
+  }
 
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormState((old) => {
+      const newData = {
+        ...old,
+        [name]: name === "email" ? value.toLowerCase() : value,
+      };
+      // localStorage.user = JSON.stringify({ ...newData, password: '' })
+      if (newData.passwordConfirm != newData.password) {
+      }
+      return newData;
+    });
+  };
 
+  const toregister = () => {
+    nav("/register");
+  };
 
-    const handleChange = (event) => {
-        const { name, value } = event.target
-        setFormState(old => {
-            const newData = { ...old, [name]: value }
-            // localStorage.user = JSON.stringify({ ...newData, password: '' })
-            if ((newData.passwordConfirm) != (newData.password)) {
-            }
-            return newData
-        })
-    }
+  const forgetPassword = () => {
+    nav("/forgetPassword");
+  };
+  let root = "accout/signInGoogle";
 
-    const toregister = () => {
-        nav('/register')
-    }
+  // console.log({formState})
+  console.log({ user });
 
-    const forgetPassword = () => {
-        nav('/forgetPassword')
-    }
-    let root = 'accout/signInGoogle'
-
-    // console.log({formState})
-    console.log({ user })
-
-    return (
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className={styles.inputSpace}>
+        <div className={styles.title}>אנגייג'ר</div>
+        <div className={styles.title2}>התחברות</div>
         <div>
-
-            <form onSubmit={handleSubmit} className={styles.inputSpace}>
-                <div className={styles.title}>אנגייג'ר</div>
-                <div className={styles.title2}>התחברות</div>
-                <div>
-
-                    <InputWrapper label={"אמייל"} setIsVisible={true} >
-                        <InputText name={'email'} required={true} onChange={handleChange} value={formState.name} className={styles.input} />
-                    </InputWrapper>
-                </div>
-
-                <div >
-                    <InputWrapper label={"סיסמה"} className={styles.nameinput}>
-                        <InputText name={'password'} required={true} onChange={handleChange} value={formState.name} className={styles.input} />
-                    </InputWrapper>
-                </div>
-                <div onClick={forgetPassword} className={styles.forget} >שכחתי סיסמה</div>
-                <button className={styles.button} type='submit' >התחברות</button>
-            </form>
-            <a href={getGoogleOAuthURL(root)} className={styles.buttongoogle}>
-                <img src="https://www.deliverlogic.com/wp-content/uploads/2021/04/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png" alt="" />
-                {/* <img src="google.png" alt="" /> */}
-                התחברות באמצעות גוגל
-            </a>
-            <div className={styles.notlogin}>
-                <div className={styles.notlogin1}>עדיין לא רשומים?</div>
-                <div onClick={toregister} className={styles.notlogin2}>הרשמה זה ממש כאן</div>
-            </div>
+          <InputWrapper label={"אמייל"} setIsVisible={true}>
+            <InputText
+              name={"email"}
+              required={true}
+              onChange={handleChange}
+              value={formState.name}
+              className={styles.input}
+            />
+          </InputWrapper>
         </div>
-    )
+
+        <div>
+          <InputWrapper label={"סיסמה"} className={styles.nameinput}>
+            <InputText
+              name={"password"}
+              required={true}
+              onChange={handleChange}
+              value={formState.name}
+              className={styles.input}
+            />
+          </InputWrapper>
+        </div>
+        <div onClick={forgetPassword} className={styles.forget}>
+          שכחתי סיסמה
+        </div>
+        <button className={styles.button} type="submit">
+          התחברות
+        </button>
+      </form>
+      <a href={getGoogleOAuthURL(root)} className={styles.buttongoogle}>
+        <img
+          src="https://www.deliverlogic.com/wp-content/uploads/2021/04/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
+          alt=""
+        />
+        {/* <img src="google.png" alt="" /> */}
+        התחברות באמצעות גוגל
+      </a>
+      <div className={styles.notlogin}>
+        <div className={styles.notlogin1}>עדיין לא רשומים?</div>
+        <div onClick={toregister} className={styles.notlogin2}>
+          הרשמה זה ממש כאן
+        </div>
+      </div>
+    </div>
+  );
 }
