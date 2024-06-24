827d2a98fd53c506f7ab6d349c2fbb2f9b63c9d4
kobikru <75983501+kobikru@users.noreply.github.com>
2024-06-24 13:54:28 +0300

    redirects

--- src/components/FeedBack/index.jsx
+++ src/components/FeedBack/index.jsx
@@ -2,35 +2,35 @@ import styles from "./style.module.css";
 import InputWrapper from "../InputWrapper";
 import InputText from "../InputText/InputText";
 import InputTextArea from "../InputTextArea/index";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import React, { useContext, useState } from "react";
-import { useParams } from 'react-router'
+import { useParams } from "react-router";
 import { useNavigate } from "react-router";
 import api from "../../functions/api";
 import DataContext from "../../context/DataContext";
 
 export default function FeedBack() {
-  const { user, setUser } = useContext(DataContext)
+  const { user, setUser } = useContext(DataContext);
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
-  const nav = useNavigate()
+  const nav = useNavigate();
 
   const handelSubmitNewFeedBack = async (e) => {
     e.preventDefault();
+    let userId = user._id;
+    let data = { title, content };
     try {
-      const response = await api.post(
-        `/feedback/${user}`,title, content);
+      const response = await api.post(`/feedback/${userId}`, data);
       toast.success(response && "נשלח בהצלחה!");
     } catch (Error) {
       console.error("Error:", Error);
       toast.error(Error?.response?.data?.msg || "something went wrong");
-
     }
   };
 
   const tohome = () => {
-    nav('/')
-  }
+    nav("/");
+  };
 
   return (
     <div className={styles.container}>
@@ -39,21 +39,35 @@ export default function FeedBack() {
         <form onSubmit={handelSubmitNewFeedBack} className={styles.inputSpace}>
           <div className={styles.title}>דברו איתנו</div>
           <div>
-            <InputWrapper label={"כותרת"} >
-              <InputText name={'title'} required={true} onChange={(e) => setContent(e.target.value)} className={styles.input} />
-            </InputWrapper>
-          </div>
-
-          <div >
-            <InputWrapper label={"תוכן ההודעה"} className={styles.nameinput}>
-              <InputTextArea name={"content"} onChange={(e) => setContent(e.target.value)} className={styles.inputaria} rows="5"
+            <InputWrapper label={"כותרת"}>
+              <InputText
+                name={"title"}
+                required={true}
+                onChange={(e) => setTitle(e.target.value)}
+                className={styles.input}
               />
             </InputWrapper>
           </div>
-          <button className={styles.button} type='submit' >שליחה</button>
+
+          <div>
+            <InputWrapper label={"תוכן ההודעה"} className={styles.nameinput}>
+              <InputTextArea
+                name={"content"}
+                onChange={(e) => setContent(e.target.value)}
+                className={styles.inputaria}
+                rows="5"
+              />
+            </InputWrapper>
+          </div>
+          <button className={styles.button} type="submit">
+            שליחה
+          </button>
         </form>
         <div className={styles.notlogin}>
-          <div onClick={tohome} className={styles.notlogin2}> חזור לדף הראשי</div>
+          <div onClick={tohome} className={styles.notlogin2}>
+            {" "}
+            חזור לדף הראשי
+          </div>
         </div>
       </div>
     </div>
