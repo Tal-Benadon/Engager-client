80a63963474c2a4bb1faa7633797bc332f771381
kobikru <75983501+kobikru@users.noreply.github.com>
2024-06-25 18:34:27 +0300

    logo

---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./style.module.css";
+
+function Logo() {
+  return (
+    <div>
+      {" "}
+      <img className={styles.eng} src="/engager.svg" alt=""></img>
+    </div>
+  );
+}
+
+export default Logo;
